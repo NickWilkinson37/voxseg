4c8e4ce2f402eff9d51d79cc72eb01710e03e706
Nick Wilkinson <nwilkinsonza@gmail.com>
2021-02-09 12:53:01 +0200

    Bugfix in main.py

--- voxseg/main.py
+++ voxseg/main.py
@@ -17,10 +17,10 @@ sess = tf.compat.v1.Session(config=session_conf)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='VAD',
                                      description='Extracts features and run VAD to generate endpoints.')
+    parser.add_argument('-m', '--model_path', type=str,
+                        help='a path to a trained vad model saved as in .h5 format, overriding the default model')
     parser.add_argument('data_dir', type=str,
                         help='a path to a Kaldi-style data directory containting \'wav.scp\', and optionally \'segments\'')
-    parser.add_argument('model_path', type=str,
-                        help='a path to a trained vad model saved as in .h5 format')
     parser.add_argument('out_dir', type=str,
                         help='a path to an output directory where the output segments will be saved')
     args = parser.parse_args()
@@ -28,7 +28,10 @@ if __name__ == '__main__':
     data = extract_feats.prep_data(args.data_dir)
     feats = extract_feats.extract(data)
     feats = extract_feats.normalize(feats)
-    model = models.load_model(args.model_path)
+    if args.model_path is not None:
+        model = models.load_model(args.model_path)
+    else:
+        model = models.load_model('voxseg/models/cnn_bilstm.h5')
     targets = run_cnnlstm.predict_targets(model, feats)
     endpoints = run_cnnlstm.decode(targets)
     run_cnnlstm.to_data_dir(endpoints, args.out_dir)
